d36a7ef3b0efc7cfa6b13b10ebf3704ff80b65b5
Nikolas-as <nikkolas21082003@gmail.com>
2023-01-30 10:49:59 -0300

    create Habits and Summary Table :hammer:

---
+++ src/components/HabitDay/index.tsx
@@ -0,0 +1,17 @@
+import { TouchableOpacity, Dimensions } from "react-native";
+
+const WEEKDAYS = 7
+const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5
+
+export const DAY_MARGIN_BETWEEN = 8
+export const DAY_SIZE: number = (Dimensions.get('screen').width / WEEKDAYS) - (SCREEN_HORIZONTAL_PADDING + 5)
+
+export function HabitDay() {
+  return (
+    <TouchableOpacity 
+      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800" 
+      style={{ width: DAY_SIZE, height: DAY_SIZE}}
+      activeOpacity={0.7}
+    />
+  );
+}
