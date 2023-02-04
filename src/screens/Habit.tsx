cdf6dadfa51c972c01f9094c4750e21c3d136133
Nikolas-as <nikkolas21082003@gmail.com>
2023-02-04 02:04:07 -0300

    Create progress bar and adding news features in Habit day :hammer:

---
+++ src/screens/Habit.tsx
@@ -0,0 +1,46 @@
+import { useRoute } from "@react-navigation/native";
+import { ScrollView, Text, View } from "react-native";
+import dayjs from 'dayjs'
+import { BackButton } from "../components/BackButton";
+import { ProgressBar } from "../components/ProgressBar";
+import { CheckBox } from "../components/Checkbox";
+
+interface HabitParams {
+  date: string
+}
+
+export function Habit() {
+  const route = useRoute()
+  const { date } = route.params as HabitParams
+
+  const parsedDate = dayjs(date)
+  const dayOfWeek = parsedDate.format('dddd')
+  const dayAndMonth = parsedDate.format('DD/MM')
+
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false} 
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
+
+        <Text className="text-white font-extrabold text-3xl">
+          {dayAndMonth}
+        </Text>
+
+        <ProgressBar progress={89} />
+
+        <View className="mt-6 pt-5">
+          <CheckBox title="Beber 2L de água" checked />
+          <CheckBox title="Dormir bem" checked={false} />
+        </View>
+
+      </ScrollView>
+    </View>
+  );
+}
